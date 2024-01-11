a4fe657ceeafecbefe453136417806a52c017a3d
wedberry <111798361+wedberry@users.noreply.github.com>
2024-01-10 19:27:43 -0500

    Create Cipherable.java

---
+++ Cipherable.java
@@ -0,0 +1,6 @@
+public interface Cipherable {
+
+	public abstract String encrypt(); //method to mask data stored in code component
+	
+	public abstract String decrypt(); //method to obtain original method from decrypted message
+}
